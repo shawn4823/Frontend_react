1d52a713ff717b5c372d8524e1dda6ce156ed64f
KIM JUNG HUN <shawn4823@gmail.com>
2026-05-14 14:07:50 +0900

    thrd commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,63 +1,64 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import './App.css'
 
-
-
-
-const scores =[
-  { 이름: "john",국어: 90, 영어:90, 수학: 80, 과학: 90 },
-  { 이름: "Peter",국어: 90, 영어:90, 수학: 80, 과학: 90 },
-  { 이름: "Susan",국어: 90, 영어:90, 수학: 80, 과학: 90 },
-  { 이름: "sue",국어: 90, 영어:90, 수학: 80, 과학: 90 },
-
-]
-// let numbers = [ 0, 1, 2, 3, 4]
-
-
-// let newScores = numbers.map(item=>{return item*2})
-// console.log(newScores)
-
-// let newScores = []
-// for(let i = 0; i < numbers.length; i++){
-//   newScores.push(numbers[i]*2)
-// }
-
-// console.log(newScores)
-
-
-
-
+const style = {
+  width:"500px",
+  height:"300px",
+  display: "flex",
+  flexDirection : "column",
+  justifyContent: "center",
+  alignItems:"center",
+}
 
 function App() {
-  
+  const handleChange = (event) => {
+   console.log (event.target.name) 
+  }
 
   return (
+    <form>
+      <div>이름: 
+      <input 
+      type="text" 
+      name="이름" 
+      value=""
+      onChange={handleChange}
+      />
+      </div>
+      <div>국어: 
+        <input 
+        type="number" 
+        name="국어" 
+        value=""
+         onChange={handleChange}
+         />
+         </div>
+      <div>영어: 
+        <input 
+        type="number" 
+        name="영어" 
+        value="" 
+        onChange={handleChange}
+        />
+        </div>
+      <div>수학: 
+        <input 
+        type="number" 
+        name="수학" 
+        value="" 
+        onChange={handleChange}
+        />
+        </div>
+      <div>과학: 
+        <input 
+        type="number" 
+        name="과학" 
+        value="" 
+        onChange={handleChange}
+        />
+      </div>
+     <button>제출</button>
+    </form>
     
-    <table>
-      
-    <tr>  
-      {
-        Object.keys(scores[0]).map(key=>(
-          <th>{key}</th>
-        ))        // scores[0]의 키값들을 배열로 반환
-      }
-      
-    </tr>
-    {scores.map(item => 
-    (
-    <tr>
-    {Object.values(item).map(
-      (value) => (<td>{value}</td>)
-    )
-    }
-    </tr>
-    )
-  )}
-  
-    </table>
   )
 }
 
